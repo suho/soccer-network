d98403848ba2aecce505dabb693d1a75c7fe82d4
hovansu <hovansu.it@gmail.com>
2016-10-10 00:19:15 +0700

    - Add Project with Swift 3.0

---
+++ SocialSoccerNetwork/Controllers/Login/LoginViewController.swift
@@ -0,0 +1,22 @@
+//
+//  LoginViewController.swift
+//  SocialSoccerNetwork
+//
+//  Created by Mylo Ho on 10/10/16.
+//  Copyright © 2016 HoVanSu. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.title = "Login"
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
