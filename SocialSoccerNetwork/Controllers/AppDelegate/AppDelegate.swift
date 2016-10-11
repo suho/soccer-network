196e3f3ce26425965e2434629919dcba11c7aa9f
hovansu <hovansu.it@gmail.com>
2016-10-11 23:24:28 +0700

    - Add Main Tab Bar with: FieldsVC, MatchsVC, ProfileVC

--- SocialSoccerNetwork/Controllers/AppDelegate/AppDelegate.swift
+++ SocialSoccerNetwork/Controllers/AppDelegate/AppDelegate.swift
@@ -12,39 +12,63 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-
+    
+    static let shared = UIApplication.shared.delegate as! AppDelegate
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         window = UIWindow(frame: UIScreen.main.bounds)
-        let loginViewController = LoginViewController(nibName: "LoginViewController", bundle: nil)
-        let loginNavigationController = UINavigationController(rootViewController: loginViewController)
-        window?.rootViewController = loginNavigationController
+        window?.rootViewController = loginViewController()
         window?.makeKeyAndVisible()
         return true
     }
-
-    func applicationWillResignActive(_ application: UIApplication) {
-        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-        // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
+    
+    func changeRootViewWhenLoginSuccess() {
+        window?.rootViewController = mainTabBarController()
     }
-
-    func applicationDidEnterBackground(_ application: UIApplication) {
-        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+    
+    func changeRootViewWhenLogout() {
+        window?.rootViewController = loginViewController()
     }
-
-    func applicationWillEnterForeground(_ application: UIApplication) {
-        // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
+    
+    func loginViewController() -> UIViewController {
+        let loginViewController = LoginViewController(nibName: "LoginViewController", bundle: nil)
+        loginViewController.title = "Login"
+        let loginNavigationController = UINavigationController(rootViewController: loginViewController)
+        return loginNavigationController
     }
-
-    func applicationDidBecomeActive(_ application: UIApplication) {
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+    
+    func mainTabBarController() -> UITabBarController {
+        let mainTabBarController: UITabBarController = UITabBarController()
+        mainTabBarController.viewControllers = [fieldsViewController(), matchsViewController(), profileViewController()]
+        mainTabBarController.selectedIndex = 1
+        return mainTabBarController
     }
-
-    func applicationWillTerminate(_ application: UIApplication) {
-        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+    
+    func fieldsViewController() -> UIViewController {
+        let fieldsViewController = FieldsViewController(nibName: "FieldsViewController", bundle: nil)
+        fieldsViewController.title = "Fields"
+        let fieldsNavigationController = UINavigationController(rootViewController: fieldsViewController)
+        fieldsNavigationController.title = fieldsViewController.title
+        fieldsNavigationController.tabBarItem.image = UIImage(named: "stadium_ic")?.withRenderingMode(.automatic)
+        return fieldsNavigationController
     }
-
-
+    
+    func matchsViewController() -> UIViewController {
+        let matchsViewController = MatchsViewController(nibName: "MatchsViewController", bundle: nil)
+        matchsViewController.title = "Matchs"
+        let matchsNavigationController = UINavigationController(rootViewController: matchsViewController)
+        matchsNavigationController.title = matchsViewController.title
+        matchsNavigationController.tabBarItem.image = UIImage(named: "football_ic")?.withRenderingMode(.automatic)
+        return matchsNavigationController
+    }
+    
+    func profileViewController() -> UIViewController {
+        let profileViewController = ProfileViewController(nibName: "ProfileViewController", bundle: nil)
+        profileViewController.title = "Profile"
+        let profileNavigationController = UINavigationController(rootViewController: profileViewController)
+        profileNavigationController.title = profileViewController.title
+        profileNavigationController.tabBarItem.image = UIImage(named: "profile_ic")?.withRenderingMode(.automatic)
+        return profileNavigationController
+    }
+    
 }
-
--- SocialSoccerNetwork/Controllers/Login/LoginViewController.swift
+++ SocialSoccerNetwork/Controllers/Login/LoginViewController.swift
@@ -9,14 +9,26 @@
 import UIKit
 
 class LoginViewController: UIViewController {
-
+    
+    // MARK:- Properties
+    @IBOutlet weak var userNameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    // MARK:- Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.title = "Login"
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    // MARK:- Action Function
+    @IBAction func signInAction(_ sender: AnyObject) {
+        AppDelegate.shared.changeRootViewWhenLoginSuccess()
+    }
+    
+    @IBAction func signUpAction(_ sender: AnyObject) {
     }
 }
